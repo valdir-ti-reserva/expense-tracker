efac458e131f84b69ff0abe54a67d93064753594
Valdir Silva <valdir.ti@gmail.com>
2020-12-08 15:57:19 -0300

    feat: create the principals components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
+
+import './App.css';
+
 function App() {
   return (
-    <div className="App">
-        Expense Tracker
+    <div>
+        <Header />
+        <div className="container">
+          <Balance />
+          <IncomeExpenses />
+          <TransactionList />
+          <AddTransaction />
+        </div>
     </div>
   );
 }

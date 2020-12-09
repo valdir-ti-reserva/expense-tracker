cb30c66162d4f17230024a60097e80d6de3a99c4
Valdir Silva <valdir.ti@gmail.com>
2020-12-09 12:11:05 -0300

    feat: List and add transaction on database

--- server/controllers/transactions.js
+++ server/controllers/transactions.js
@@ -16,7 +16,7 @@ exports.getTransactions = async (req, res, next) => {
 
   } catch (err) {
 
-    return res.send(500).json({
+    return res.status(500).json({
       success: false,
       error: 'Server Error'
     });
@@ -28,7 +28,40 @@ exports.getTransactions = async (req, res, next) => {
 // @route  POST /api/v1/transactions
 // @access Public
 exports.addTransaction = async (req, res, next) => {
-  res.send('POST Transactions');
+  
+  try {
+  
+    const {text, amount} = req.body;
+
+    const transaction = await Transaction.create(req.body);
+
+    return res.status(200).json({
+      success: true,
+      data: transaction
+    });
+    
+  } catch (err) {
+  
+    if(err.name === 'ValidationError'){
+
+      const messages = Object.values(err.errors).map(val => val.message);
+
+      return res.status(400).json({
+        success: false,
+        error: messages
+      });
+
+    }else{
+      
+      return res.status(500).json({
+        success: false,
+        error: 'Server Error'
+      });
+
+    }
+   
+  }
+
 }
 
 // @desc   Delete Transaction

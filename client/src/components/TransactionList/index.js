d04fa2173aa4effb1c8110541dea8cf73c96377e
Valdir Silva <valdir.ti@gmail.com>
2020-12-09 12:49:53 -0300

    feat: list transactions from database

--- client/src/components/TransactionList/index.js
+++ client/src/components/TransactionList/index.js
@@ -1,17 +1,22 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { GlobalContext } from '../../context/GlobalState';
 
 import {Transaction} from '../Transaction'
 
 export const TransactionList = () => {
 
-  const { transactions } = useContext(GlobalContext);
+  const { transactions, getTransactions } = useContext(GlobalContext);
+
+  useEffect(() => {
+    getTransactions();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
        <h3>History</h3>
         <ul id="list" className="list">
-          {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction}/>))}          
+          {transactions.map(transaction => (<Transaction key={transaction._id} transaction={transaction}/>))}          
         </ul>
     </>
   )

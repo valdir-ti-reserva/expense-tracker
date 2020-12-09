3fb501441e08c99a882e7c8854ef7093db4fe186
Valdir Silva <valdir.ti@gmail.com>
2020-12-09 11:56:45 -0300

    feat: get all transactions from database

--- server/controllers/transactions.js
+++ server/controllers/transactions.js
@@ -1,20 +1,39 @@
+const Transaction = require('../models/Transaction');
+
 // @desc   Get All Transactions
 // @route  GET /api/v1/transactions
 // @access Public
-exports.getTransactions = (req, res, next) => {
-  res.send('GET Transactions');
+exports.getTransactions = async (req, res, next) => {
+  try {
+    
+    const transactions = await Transaction.find();
+    
+    return res.status(200).json({
+      success: true,
+      count: transactions.length,
+      data: transactions
+    });
+
+  } catch (err) {
+
+    return res.send(500).json({
+      success: false,
+      error: 'Server Error'
+    });
+
+  }
 }
 
 // @desc   Add Transaction
 // @route  POST /api/v1/transactions
 // @access Public
-exports.addTransaction = (req, res, next) => {
+exports.addTransaction = async (req, res, next) => {
   res.send('POST Transactions');
 }
 
 // @desc   Delete Transaction
 // @route  DELETE /api/v1/transactions/:id
 // @access Public
-exports.deleteTransaction = (req, res, next) => {
+exports.deleteTransaction = async (req, res, next) => {
   res.send('DELETE Transactions');
 }

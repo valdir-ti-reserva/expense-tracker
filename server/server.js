73c32137eb2d85ef4f7396780ad26e2b7006d5d4
Valdir Silva <valdir.ti@gmail.com>
2020-12-09 10:41:34 -0300

    feat: create transactions routes

--- server/server.js
+++ server/server.js
@@ -5,9 +5,11 @@ const morgan = require('morgan');
 
 dotenv.config({ path: './config/config.env' });
 
+const transacations = require('./routes/transactions');
+
 const app = express();
 
-app.get('/', (req, res)=>res.send('Hello'));
+app.use('/api/v1/transactions', transacations);
 
 const PORT = process.env.PORT || 5000;
 
